ab50143b298b76ec76224514673626659fa2d579
Adomas02 <kazen.adomas@gmail.com>
2022-02-11 16:28:17 +0200

    Padarytas skaiciavimas su mediana
    Prideta rikiavimo funkcija ir skaiciavimo su mediana funkcija. Truksta sutvarkyti isvedima

--- Source.cpp
+++ Source.cpp
@@ -10,52 +10,98 @@ using std::setw;
 using std::fixed;
 using std::setprecision;
 
-struct data {
+struct Mokinys {
 	string vardas, pavarde;
-	int paz[3], egz,kiek;
+	int* paz;
+	int egz, kiek;
 	double rezult = 0;
 };
-void ivestis(data& temp);
-void isvestis(data& temp);
-double skaiciavimas(data& temp);
+void ivestis(Mokinys& mok);
+void isvestis(Mokinys& mok);
+double skaiciavimas(double a, Mokinys& mok);
+double skaiciavimasVid(Mokinys& mok);
+void rikiavimas(Mokinys& mok);
+double skaiciavimasMed(Mokinys& mok);
+
 int main()
 {
-	data a;
+	Mokinys a;
 	ivestis(a);
+	rikiavimas(a);
 	isvestis(a);
-	
+
 }
-void ivestis(data& temp)
+void ivestis(Mokinys& mok)
 {
 	cout << "Iveskite mokinio varda ";
-	cin >> temp.vardas;
+	cin >> mok.vardas;
 	cout << "Iveskite mokinio pavarde ";
-	cin >> temp.pavarde;
+	cin >> mok.pavarde;
 	cout << "Iveskite pazymiu skaiciu ";
-	cin >> temp.kiek;
-	cout << "Iveskite pazymius"<<endl;
-	for (int i = 0;i < temp.kiek;i++)
+	cin >> mok.kiek;
+	cout << "Iveskite pazymius" << endl;
+
+	mok.paz = new int[mok.kiek];
+
+	for (int i = 0;i < mok.kiek;i++)
 	{
-		cin >> temp.paz[i];
+		cin >> mok.paz[i];
 	}
 	cout << "Iveskite egzamino pazymi ";
-	cin >> temp.egz;
+	cin >> mok.egz;
 }
-void isvestis(data& temp)
+void isvestis(Mokinys& mok)
 {
-	cout <<"Vardas" << setw(10) <<"Pavarde" << setw(20) <<"Galutinis (Vid.)"<<endl;
-	cout << "--------------------------------------"<<endl;
-	cout << temp.vardas << setw(10) << temp.pavarde<<setw(10)<<fixed<<setprecision(2)<<skaiciavimas(temp);
+	cout << "Vardas" << setw(10) << "Pavarde" << setw(20) << "Galutinis (Vid.)" << setw(20) <<"Galutinis (Med.)" << endl;
+	cout << "--------------------------------------" << endl;
+	cout << mok.vardas << setw(10) << mok.pavarde << setw(10) << fixed << setprecision(2) << skaiciavimasVid(mok)<< setw(10) << skaiciavimasMed(mok) << endl;
+
+
+
 }
-double skaiciavimas(data& temp)
+double skaiciavimasVid(Mokinys& mok)
 {
-	double sum=0,vid;//pazymiu suma, vidurkis ir galutinis
-	for (int i = 0;i < temp.kiek;i++)
+	double sum = 0, vid;//pazymiu suma, vidurkis ir galutinis
+	for (int i = 0;i < mok.kiek;i++)
 	{
-		sum += temp.paz[i];
+		sum += mok.paz[i];
 	}
-	vid = sum / temp.kiek;
-	temp.rezult = 0.4 * vid + 0.6 * temp.egz;
-	return temp.rezult;
-	
+	vid = sum / mok.kiek;
+	mok.rezult=skaiciavimas(vid,mok);
+	return mok.rezult;
+}
+void rikiavimas(Mokinys& mok)
+{
+	int laikinas;
+	for (int i = 0;i < mok.kiek; i++)
+		for (int j = i + 1;j < mok.kiek;j++)
+		{
+			if (mok.paz[i] > mok.paz[j])
+			{
+				laikinas = mok.paz[i];
+				mok.paz[i] = mok.paz[j];
+				mok.paz[j] = laikinas;
+			}
+
+		}
+}
+double skaiciavimas(double a,Mokinys& mok)
+{
+	mok.rezult = 0.4 * a + 0.6 * mok.egz;
+	return mok.rezult;
+}
+double skaiciavimasMed(Mokinys& mok)
+{
+	if (mok.kiek % 2 == 0)
+	{
+		double laikMed;
+		laikMed =( (mok.paz[mok.kiek / 2] + mok.paz[mok.kiek / 2 - 1]) / 2.0);
+		mok.rezult = skaiciavimas(laikMed, mok);
+		return mok.rezult;
+	}
+	else
+	{
+		mok.rezult = skaiciavimas(mok.paz[mok.kiek/2], mok);
+		return mok.rezult;
+	}
 }
