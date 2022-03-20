f9514a7fb0675ffb1bdbf3a17982541012f976b4
Adomas02 <kazen.adomas@gmail.com>
2022-03-20 16:00:46 +0000

    Prdietas issimciu valdymas

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,32 @@
 
 int main()
 {
+	
+	std::ifstream open_f("studentai.txt");
+	/*if(open_f.peek() == std::ifstream::traits_type::eof())
+	{
+		cout<<"Tuscia";
+	}
+	*/
+	try{
+		if(!open_f){
+			throw 1;
+		}
+	}catch(int x){
+		cout<<"Failas neegzistuoja"<<endl;
+		return 0;		
+	}
+
+	try{
+		if(open_f.peek() == std::ifstream::traits_type::eof()){
+			throw 2;
+		}
+	}catch(int x){
+		cout<<"Failas tuscias"<<endl;
+		return 0;
+	}
+
+
 	srand(time(NULL));
 	std::vector<Mokinys> mok;
 	//Mokinys a;
