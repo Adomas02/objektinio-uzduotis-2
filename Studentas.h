5498daef42946da1eeeee5eba498656464e32b2d
Adomas02 <kazen.adomas@gmail.com>
2022-05-08 15:35:06 +0300

    studentas klase

--- Studentas.h
+++ Studentas.h
@@ -2,6 +2,9 @@
 #include<iomanip>
 #include<vector>
 
+
+
+
 class Studentas {
 // realizacija
 private:
@@ -9,16 +12,24 @@ private:
   std::string pavarde_;
   double egzaminas_;
   std::vector<double> nd_;
+  
 // interfeisas
 public:
     void setVardas (std::string aVardas){ vardas_=aVardas; }
     void setPavarde (std::string aPavarde){ pavarde_=aPavarde; }
     void setEgzaminas (double aEgzaminas){ egzaminas_ = aEgzaminas;}
-    
-    std::string getVardas(){ return vardas_; }
+    void setNd (std::vector<double> aNd){ for(auto el:aNd){nd_.push_back(el);}}
+   
+
+    inline std::string getVardas() const { return vardas_; }
     std::string getPavarde(){ return pavarde_; }
     double getEgzaminas(){ return egzaminas_; }
+    std::vector<double> getNd(){return nd_;}
+   
+
     Studentas() : egzaminas_(0) { }  // default konstruktorius
+ 
     Studentas(std::istream& is);
-  std::istream& readStudent(std::istream&);  // set'eriai
+    std::istream& readStudent(std::istream&);  // set'eriai
 };
+
--- testClass.cpp
+++ testClass.cpp
@@ -4,6 +4,7 @@
 
 int main(){
     Studentas mokinys;
+    std::vector<double> temp;
 
     mokinys.setVardas("Adomas");
     std::cout<<mokinys.getVardas();
@@ -11,5 +12,15 @@ int main(){
     std::cout<<mokinys.getPavarde();
     mokinys.setEgzaminas(10);
     std::cout<<mokinys.getEgzaminas();
+    double paz;
+    std::cout<<std::endl;
+    for(int i=0;i<3;i++)
+    {
+        std::cin>>paz;
+        temp.push_back(paz);
+    }
+    mokinys.setNd(temp);
+    //std::cout<<temp[0];
     
+   
 }
